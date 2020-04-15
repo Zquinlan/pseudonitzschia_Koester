1ae9e2629d7ba628ff7f9f001c8b3962bd3a53f9
irinakoester <42815817+irinakoester@users.noreply.github.com>
2020-04-15 16:06:47 -0700

    Saved Classyfire code
    not sure if I saved all changes...just in case...

--- Code/Rcode_InchiforClassyfire_Pn.R
+++ Code/Rcode_InchiforClassyfire_Pn.R
@@ -47,17 +47,15 @@ LibIDs_combined <- LibIDs%>%
   mutate(inchi_combined = case_when(INCHI_library != "N/A" ~ INCHI_library,
                                     INCHI_library == "N/A" ~ INCHI_analog))
 
-##replace VZFRNCSOCOPNDB-AOKDLOFSSA-N
-##with InChI=1S/C15H21NO6/c1-8(4-3-5-9(2)14(19)20)11-7-16-13(15(21)22)10(11)6-12(17)18/h3-5,9-11,13,16H,6-7H2,1-2H3,(H,17,18)(H,19,20)(H,21,22)/b5-3+,8-4-/t9-,10+,11-,13+/m1/s1
-#mutate(inchi_combined = gsub("VZFRNCSOCOPNDB-AOKDLOFSSA-N", "InChI=1S/C15H21NO6/c1-8(4-3-5-9(2)14(19)20)11-7-16-13(15(21)22)10(11)6-12(17)18/h3-5,9-11,13,16H,6-7H2,1-2H3,(H,17,18)(H,19,20)(H,21,22)/b5-3+,8-4-/t9-,10+,11-,13+/m1/s1", inchi_combined))
-
-
-#if it doesn't start with "InChi=" add "InChi=" (except N/A)
+#if it doesn't start with "InChi=" add "InChi=" to fix the few wrong Inchi which would fail in Classyfire
+LibIDs_combined_inchifixed <- LibIDs_combined%>%
+  mutate(inchi_combined = case_when(!inchi_combined %like% "InChI=%" ~ paste("InChI=", inchi_combined, sep = ""),
+                           TRUE ~ as.character(inchi_combined)))
 
 #convert inchi of combined table to inchikey for Classyfire
-LibIds_inchikeys <- LibIDs_combined%>%
+Combined_classyfire <- LibIDs_combined_inchifixed%>%
   select(`#Scan#`, inchi_combined)%>%
-  filter(inchi_combined != "N/A")%>%
+  filter(inchi_combined != "InChI=N/A")%>%
   group_by(`#Scan#`)%>%
   filter(row_number(inchi_combined) == 1 )%>%
   mutate(inchi_key = cs_inchi_inchikey(inchi_combined))%>%
@@ -65,23 +63,9 @@ LibIds_inchikeys <- LibIDs_combined%>%
 
 ## write file to use for Classyfire at https://cfb.fiehnlab.ucdavis.edu/
 
-write_csv(LibIds_inchikeys,"Pn_GNPS_combined_Inchikeys.csv")
+write_csv(Combined_classyfire,"./Raw/Pn_GNPS_combined_Inchikeys.csv")
 
 
 
 
 
-##example
-inchi <-  "InChI=1xxS/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-
-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13-,16-,17-/m0/s1"
-inchi <-  "1xxS/C17H19NO3/c1-18-7-6-17-10-3-5-13(20)16(17)21-15-12(19)4-
-2-9(14(15)17)8-11(10)18/h2-5,10-11,13,16,19-20H,6-8H2,1H3/t10-,11+,13-,16-,17-/m0/s1"
-# convert InChI to CSID
-cs_inchi_inchikey(inchi)
-
-
-write_csv(LibIDs_combined,"./Analyzed/Pn_GNPS_Inchikeys_combined.csv")
-
-###for just one table it would be:
-
-failednaming <- read_csv("./Analyzed/Inchi_failednaming.csv")
